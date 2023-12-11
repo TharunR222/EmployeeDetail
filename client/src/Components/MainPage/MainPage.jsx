4defcf746b2d15c0759dc101a0c4a8b3dd4cd52c
Tharun R <rtharun222@gmail.com>
2023-12-11 21:38:17 +0530

    Initial Commit

---
+++ client/src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,145 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import './MainPage.css'
+
+const MainPage = () => {
+
+  const [formData, setFormData] = useState({ eid: '', fname: '', lname: '', dept: '', desig: '', salary: '', dob: '' });
+  const [showData, setShowData] = useState([]);
+  const [editingItem, setEditingItem] = useState(false);
+
+  const handleEdit = async (e, p1) => {
+    setEditingItem(true);
+    let response = await fetch(`http://127.0.0.1:5000/updData/${p1}`, {
+      method: 'GET'
+    });
+    let res = await response.json();
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      ...res
+    }));
+  }
+
+  const handleDelete = async (e, p1) => {
+    let response = await fetch(`http://127.0.0.1:5000/updData/${p1}`, {
+      method: 'DELETE'
+    });
+    let res = await response.json();
+    setShowData(res.data);
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((prevFormData) => ({
+      ...prevFormData, [name]: value
+    }))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var value = true;
+    var valid = true;
+    for (const key in formData) {
+      if (`${formData[key]}`.trim() == '') {
+        value = false;
+        setFormData((prevFormData) => ({
+          ...prevFormData, [key]: ''
+        }))
+      } else if ((key != 'dob' && key != 'salary' && key != 'eid') && `${formData[key]}`.match(/[^a-zA-Z\s]+$/) != null) {
+        valid = false;
+        alert(`${key} must contain only alphabets`);
+        setFormData((prevFormData) => ({
+          ...prevFormData, [key]: ''
+        }))
+      } else if ((key == 'salary' || key == 'eid') && `${formData[key]}`.match(/^[0-9]+$/) == null) {
+        valid = false;
+        alert(`${key} must contain only numbers`);
+        setFormData((prevFormData) => ({
+          ...prevFormData, [key]: ''
+        }))
+      }
+    }
+    if (value == false) {
+      alert("Please fill out the blank fields");
+    }
+    if (value && valid) {
+      console.log(formData);
+      const func = async () => {
+        let response = await fetch("http://127.0.0.1:5000/postData", {
+          method: editingItem ? 'PUT' : 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData)
+        });
+        let res = await response.json();
+        return res;
+
+      }
+      func()
+        .then(res => {
+          setShowData(res.data);
+        })
+        .then(res => {
+          for (const key in formData) {
+            setFormData((prevFormData) => ({ ...prevFormData, [key]: '' }))
+          }
+          setEditingItem(false);
+        });
+    }
+  };
+
+  return (
+    <div className='main-block'>
+      <div className="form-block">
+        <h2>Employee Detail Entry</h2>
+        <form onSubmit={handleSubmit}>
+          Employee ID:<input type='text' name='eid' value={formData.eid} onChange={handleChange} required />
+          First Name:<input type='text' name='fname' value={formData.fname} onChange={handleChange} required />
+          Last Name:<input type='text' name='lname' value={formData.lname} onChange={handleChange} required />
+          Department:<input type='text' name='dept' value={formData.dept} onChange={handleChange} required />
+          Designation:<input type='text' name='desig' value={formData.desig} onChange={handleChange} required />
+          Salary:<input type='text' name='salary' value={formData.salary} onChange={handleChange} required />
+          DOB:<input type='date' name='dob' value={formData.dob} onChange={handleChange} required />
+          <input type='submit' className='submit' />
+        </form>
+      </div>
+      <div className="show-block">
+        <h2>Employee Detail</h2>
+        <table>
+          <tr>
+            <th>Employee ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Department</th>
+            <th>Designation</th>
+            <th>Salary</th>
+            <th>Date of Birth</th>
+            <th>Changes</th>
+          </tr>
+          {
+            showData.length > 0 &&
+            showData.map((element, index) => {
+              return (<tr key={index}>
+                <td>{element.eid}</td>
+                <td>{element.fname}</td>
+                <td>{element.lname}</td>
+                <td>{element.dept}</td>
+                <td>{element.desig}</td>
+                <td>{element.salary}</td>
+                <td>{element.dob}</td>
+                <td className='change-cell'>
+                  <button className='edit' onClick={e => handleEdit(e, element.eid)}>Edit</button>
+                  <button className='delete' onClick={e => handleDelete(e, element.eid)}>Delete</button>
+                </td>
+              </tr>)
+            })
+          }
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
---
+++ server/server.py
@@ -0,0 +1,128 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+from flask_cors import CORS
+import psycopg2
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+class UpdateData(Resource):
+    def get(self, eid):
+        try:
+            conn = psycopg2.connect(database='empdet', user='postgres',
+                                    password='postgresRoot', host='localhost', port='5432')
+            curr = conn.cursor()
+            curr.execute(
+                "SELECT * FROM employeeDetails WHERE eid = %s", (eid,))
+            row = curr.fetchone()
+
+            if row:
+                return {'eid': row[0], 'fname': row[1], 'lname': row[2], 'dept': row[3],
+                        'desig': row[4], 'salary': row[5], 'dob': str(row[6])}
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error with the database:", error)
+        finally:
+            if conn:
+                curr.close()
+                conn.close()
+
+    def delete(self, eid):
+        try:
+            conn = psycopg2.connect(database='empdet', user='postgres',
+                                    password='postgresRoot', host='localhost', port='5432')
+            curr = conn.cursor()
+            curr.execute(
+                "DELETE FROM employeeDetails WHERE eid = %s", (eid,))
+            conn.commit()
+            curr.execute("SELECT * FROM employeeDetails")
+            result = curr.fetchall()
+            data = []
+            if result:
+                for row in result:
+                    data.append({'eid': row[0], 'fname': row[1], 'lname': row[2], 'dept': row[3],
+                                'desig': row[4], 'salary': row[5], 'dob': str(row[6])})
+                return {'data': data}
+        except (Exception, psycopg2.Error) as error:
+            print("Error with the database:", error)
+        finally:
+            if conn:
+                curr.close()
+                conn.close()
+
+
+class PostData(Resource):
+    def post(self):
+        args = request.json
+        try:
+            conn = psycopg2.connect(database='empdet', user='postgres',
+                                    password='postgresRoot', host='localhost', port='5432')
+            curr = conn.cursor()
+            curr.execute("INSERT INTO employeeDetails(eid, fname, lname, dept, desig, salary, dob) VALUES(%s, %s, %s, %s, %s, %s, %s)",
+                         (int(args['eid']), args['fname'], args['lname'], args['dept'], args['desig'], int(args['salary']), args['dob']))
+
+            if curr.rowcount != 0:
+                conn.commit()
+                curr.execute("SELECT * FROM employeeDetails")
+                result = curr.fetchall()
+                data = []
+                if result:
+                    for row in result:
+                        data.append({'eid': row[0], 'fname': row[1], 'lname': row[2], 'dept': row[3],
+                                    'desig': row[4], 'salary': row[5], 'dob': str(row[6])})
+                    return {'data': data}
+            else:
+                return {'status': False}
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error with the database:", error)
+        finally:
+            if conn:
+                curr.close()
+                conn.close()
+
+    def put(self):
+        args = request.json
+        try:
+            conn = psycopg2.connect(database='empdet', user='postgres',
+                                    password='postgresRoot', host='localhost', port='5432')
+            curr = conn.cursor()
+            curr.execute("""UPDATE employeeDetails 
+                         SET 
+                         eid = %s, 
+                         fname = %s, 
+                         lname = %s, 
+                         dept = %s, 
+                         desig = %s, 
+                         salary = %s, 
+                         dob = %s 
+                         WHERE
+                         eid = %s
+                         """,
+                         (int(args['eid']), args['fname'], args['lname'], args['dept'], args['desig'], int(args['salary']), args['dob'], args['eid']))
+
+            conn.commit()
+            curr.execute("SELECT * FROM employeeDetails")
+            result = curr.fetchall()
+            data = []
+            if result:
+                for row in result:
+                    data.append({'eid': row[0], 'fname': row[1], 'lname': row[2], 'dept': row[3],
+                                'desig': row[4], 'salary': row[5], 'dob': str(row[6])})
+                return {'data': data}
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error with the database:", error)
+        finally:
+            if conn:
+                curr.close()
+                conn.close()
+
+
+api.add_resource(PostData, '/postData')
+api.add_resource(UpdateData, '/updData/<int:eid>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
