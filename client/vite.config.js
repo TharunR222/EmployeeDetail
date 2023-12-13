d32ab3ae44596a62215536a538d8eea4c29b0c15
Tharun R <rtharun222@gmail.com>
2023-12-13 21:36:12 +0530

    Dockerized

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      polling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 5173
+  }
 })
